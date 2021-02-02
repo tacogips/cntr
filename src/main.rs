70ad96d5e5526c52bd7aee498bffcc5b822c9ef1
tacogips <me@tacogips.me>
2021-02-03 00:11:13 +0900

    version up

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::fs::OpenOptions;
 use std::io::Read;
 
 #[derive(Clap, Debug)]
-#[clap(version = "0.2.0", author = "tacogips")]
+#[clap(version = "0.2.2", author = "tacogips")]
 struct Opts {
     #[clap(short, long)]
     dest_file: String,
